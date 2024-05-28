d123aa723c4f508ec2b68f635040727a880b6ef0
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-05-28 09:52:28 -0300

    Add password

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,34 @@ class ValidaFormulario {
 
     handleSubmit(e) {
         e.preventDefault(); //p/ nao enviar o formulário
-        const camposValida = this.camposValidos();
+        const camposValidos = this.camposValidos();
     }
 
     camposValidos() {
-        let valida = true;
+        let valid = true;
+
+        for(let errorText of this.formulario.querySelectorAll('.error-text')){
+            error.Text.remove();
+        }
+
+        for(let campo of this.formulario.querySelectorAll('.validar')){
+            const label = campo.previousElementSibling.innerText;
+
+            if(!campo.value){
+                this.criaErro(campo, `Campo ${label} não pode estar em branco.`);
+                valid = false; 
+            }
+        }
+    }
+
+    criaErro(campo, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('error-text');
+
     }
 }
+
+
 
 const valida = new ValidaFormulario();
